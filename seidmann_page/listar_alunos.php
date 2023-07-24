9a4d4f95ac3f3c1a89188208eefa2e5217783b62
erickseidmann <erickseidmann@gmail.com>
2023-07-24 19:44:56 -0300

    ajustando lista

--- seidmann_page/listar_alunos.php
+++ seidmann_page/listar_alunos.php
@@ -2,35 +2,192 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Lista de Alunos</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v5.6.8, mobirise.com">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:image:src" content="">
+  <meta property="og:image" content="">
+  <meta name="twitter:title" content="Blog">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <title>Lista de Alunos</title>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pkubebP0x8l7s5bq4XgzLgvs5pP5Dpr8U0I6k7+4JcYKzzw3k0CGPlFElN4Q8RzO" crossorigin="anonymous"></script>
+  <link rel="shortcut icon" href="assets/images/whatsapp-image-2022-09-13-at-14.34.33-121x121.jpg" type="image/x-icon">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <meta name="description" content="">
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+  <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap"></noscript>
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  <link rel="stylesheet" href="/seidmann_page/style/style.css">
+  <script type="text/javascript" src = "https://ajax.aspnetcdn.com/ajax/jquery.ui/1.12.1/jquery-ui.min.js"> </script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+ 
+  <script defer src="script.js"></script>
+  <link rel="stylesheet" href="styles.css">
+    
 </head>
 <body>
+<section data-bs-version="5.1" class="menu cid-tkzhgMMrxs" once="menu" id="menu1-x">
+    <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
+      <div class="container">
+        
+        <div class="navbar-brand">
+          <span class="navbar-logo">
+            <a href="https://mobiri.se">
+              <img src="assets/images/whatsapp-image-2022-09-13-at-14.34.33-121x121.jpg" alt="Mobirise Website Builder" style="height: 3.8rem;">
+            </a>
+          </span>
+          <span class="navbar-caption-wrap">
+            <a class="navbar-caption text-black text-primary display-7" href="http://seidmanninstitute.com/">SEIDMANN INSTITUTE</a>
+          </span>
+        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <div class="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
+            <li class="nav-item">
+              <a class="nav-link link text-black text-primary display-4" href="index.html">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link link text-black text-primary display-4" href="page1.html">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link link text-black text-primary display-4" href="horarios.html">Agenda</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link link text-black text-primary display-4" href="formularios.html">Formulários</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link link text-black text-primary display-4" href="listar_alunos.php">listar</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </section>
+  <section data-bs-version="5.1" class="info3 cid-tkzhgHSUx7" id="info3-t">
+    <div class="mbr-overlay" style="opacity: 0.6; background-color: rgb(53, 53, 53);"></div>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="card col-12 col-lg-10">
+          <div class="card-wrapper">
+            <div class="card-box align-center">
+              <h4 class="card-title mbr-fonts-style align-center mb-4 display-1"><strong>Inglês para alcançar o mundo!</strong></h4>
+              <div class="mbr-section-btn mt-3">
+                <a class="btn btn-warning-outline display-4" href="https://wa.me/5519988279707" target="_blank">Falar com a Direção!</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section data-bs-version="5.1" class="features3 cid-tkzhgIxW41 col-md-4 text-center" id="features3-u">
+  <div class="container">
+  <div class="row input-group col-md-20 fw-semibold ">
+              <div class="col-md-18 ">
+
     <h1>Lista de Alunos</h1>
+
     <?php
-    // Inclua o arquivo de configuração do banco de dados
-    require_once "config.php";
+        // Inclua o arquivo de configuração do banco de dados
+        require_once "config.php";
 
-    // Consulta todos os alunos na tabela "formulario"
-    $sql = "SELECT * FROM formulario";
-    $result = $conn->query($sql);
+        // Consulta todos os alunos na tabela "formulario"
+        $sql = "SELECT * FROM formulario";
+        $result = $conn->query($sql);
 
-    if ($result->num_rows > 0) {
-        echo "<ul>";
-        // Loop para exibir cada aluno na lista
-        while ($row = $result->fetch_assoc()) {
-            echo "<li>";
-            echo "Nome: " . $row["nome_completo"] . " | Status: " . ($row["status"] == 1 ? "Ativo" : "Inativo");
-            echo " | <a href='editar_aluno.php?id=" . $row["id"] . "'>Editar</a>";
-            echo " | <a href='excluir_aluno.php?id=" . $row["id"] . "'>Excluir</a>";
-            echo "</li>";
+        if ($result->num_rows > 0) {
+            echo '<ul class="list-group">';
+            // Loop para exibir cada aluno na lista
+            while ($row = $result->fetch_assoc()) {
+                $status = $row["status"] == 1 ? "Ativo" : "Inativo";
+                echo '<li class="list-group-item list-group-item-action list-group-item-primary text-center align-items-center">';
+                echo 'Nome: ' . $row["nome_completo"] . ' | Status: ' . $status;
+                echo ' | <a class="btn btn-primary btn-sm" href="editar_aluno.php?id=' . $row["id"] . '">Editar</a>';
+                echo ' | <a class="btn btn-danger btn-sm" href="excluir_aluno.php?id=' . $row["id"] . '">Excluir</a>';
+                echo '</li>';
+            }
+            echo '</ul>';
+        } else {
+            echo '<div class="text-center">Nenhum aluno cadastrado.</div>';
         }
-        echo "</ul>";
-    } else {
-        echo "Nenhum aluno cadastrado.";
-    }
 
-    // Fecha a conexão com o banco de dados
-    $conn->close();
-    ?>
+        // Fecha a conexão com o banco de dados
+        $conn->close();
+        ?>
+        </div>
+        </div>
+        </div>
+    </section>    
+    
+<section data-bs-version="5.1" class="footer3 cid-s48P1Icc8J" once="footers" id="footer3-i">
+
+    
+
+    
+
+<div class="container">
+    <div class="media-container-row align-center mbr-white">
+        <div class="row row-links">
+            <ul class="foot-menu">
+                
+                
+                
+                
+                
+            <li class="foot-menu-item mbr-fonts-style display-7">Email</li><li class="foot-menu-item mbr-fonts-style display-7">Instagram</li><li class="foot-menu-item mbr-fonts-style display-7">Facebook</li></ul>
+        </div>
+        <div class="row social-row">
+            <div class="social-list align-right pb-2">
+                
+                
+                
+                
+                
+                
+            <div class="soc-item">
+                    <a href="http://seidmanninstitute.com/" target="_blank">
+                        <span class="mbr-iconfont mbr-iconfont-social mobi-mbri-arrow-next mobi-mbri"></span>
+                    </a>
+                </div><div class="soc-item">
+                    <a href="https://www.facebook.com/Seidmanninstitute" target="_blank">
+                        <span class="mbr-iconfont mbr-iconfont-social socicon-facebook socicon"></span>
+                    </a>
+                </div><div class="soc-item">
+                    <a href="https://www.youtube.com/channel/UCrVxXG9Z2TqdokA7ST91bcA" target="_blank">
+                        <span class="mbr-iconfont mbr-iconfont-social socicon-youtube socicon"></span>
+                    </a>
+                </div><div class="soc-item">
+                    <a href="https://www.instagram.com/seidmann_institute/" target="_blank">
+                        <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon"></span>
+                    </a>
+                </div></div>
+        </div>
+        <div class="row row-copirayt">
+            <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">
+                © Seidmann Institute All Rights Reserved.
+            </p>
+        </div>
+    </div>
+</div>
+</section><section class="display-7" ><p hidden href="https://mobiri.se/2782058" ></a><p hidden> &#8204;</p><a  href="https://mobirise.com/offline-website-builder.html"></a></section><script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>  <script src="assets/smoothscroll/smooth-scroll.js"></script>  <script src="assets/ytplayer/index.js"></script>  <script src="assets/embla/embla.min.js"></script>  <script src="assets/embla/script.js"></script>  <script src="assets/dropdown/js/navbar-dropdown.js"></script>  <script src="assets/theme/js/script.js"></script>  
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>  
+
 </body>
 </html>
