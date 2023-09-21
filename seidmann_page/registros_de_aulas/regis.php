d2db890dd85a0eb967938837471b2bc2200ce6f4
erickseidmann <erickseidmann@gmail.com>
2023-09-20 22:37:50 -0300

    novos estilos css

--- seidmann_page/registros_de_aulas/regis.php
+++ seidmann_page/registros_de_aulas/regis.php
@@ -1,32 +1,51 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Seidmann</title>
-    <meta name="author" content="Erick Seidmann">
-    <meta name="description" content="pagina de registros de aulas feitas pelos professores">
-    <link rel="stylesheet" href="styles/stylesreg.css">
+<html>
+<?php
+include('header.php'); // Inclui o cabeçalho
+?>
 
-</head>
 <body>
-    <header>
-            
-    </header>
 
-    <nav class="sidebar">
-        <ul>
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Registrar</a></li>
-            <li><a href="#">Ver Registros</a></li>
-            <li><a href="#">Agenda de Aulas</a></li>
-            <li><a href="#">Treinamentos</a></li>
-            <li><a href="#">Homework</a></li>
-            <li><a href="#">Trabalhos</a></li>
-            <li><a href="#">Provas</a></li>
-        </ul>
-    </nav>
+<main>
+<section id="formReg">
+    
+  
+  <form id="form_reg" class="container" action="processar_registro.php" method="post">
+
+        <h1>Registros de Aulas</h1>
+
+        <div class=" ">
+            <label for="aluno">Student:</label>
+                <select name="aluno" id="aluno" class="form-select bg-light" onchange="buscarEmail(0)">
+                    <option value="">Selecione um aluno</option>
+                        <?php
+                        require_once "config.php";
+                        $sql = "SELECT nome_completo, email FROM formulario";
+                        $result = $conn->query($sql);
+                        $alunos = array();
+                        while ($row = $result->fetch_assoc()) {
+                            $nomeAluno = $row['nome_completo'];
+                            $emailAluno = $row['email'];
+                            echo "<option value='$nomeAluno'>$nomeAluno</option>";
+                            $alunos[$nomeAluno] = $emailAluno;
+                        }
+                        ?>
+                </select>
+            <label for="email">Email:</label>
+
+                <input type="email" name="email" id="email" class="form-control bg-light" readonly>
+                <input id="botaoEnviar" class="btn btn-primary" type="submit" value="Registrar">
+        </div>
+
+    </form>
+
+    <script>
+        var alunosEEmails = <?php echo json_encode($alunos); ?>;
+    </script>
+    </section>
 
 
+</main>
+<script src="scripts/reg.js"></script>
 </body>
 </html>
